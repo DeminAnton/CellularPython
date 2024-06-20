e55ccf039de49b7e0aea677ad62116e94160993e
DeminAnton <demantser@gmail.com>
2024-06-20 13:09:55 +0800

    Genetic alg revew

--- Lessons/3/config.py
+++ Lessons/3/config.py
@@ -2,12 +2,12 @@ from dataclasses import dataclass
 
 @dataclass
 class Config:
-    grid_rows = 100
-    grid_columns = 150
+    grid_rows = 180
+    grid_columns = 260
     scale = 4
     x_res = grid_columns * scale
     y_res = grid_rows * scale
     live_color = (0, 0, 0)
     death_color = (255, 255, 255)
     FPS = 50
-    init_prob = 0.5
+    init_prob = 0.1
--- Lessons/3/test.py
+++
@@ -1,3 +0,0 @@
-from grid import GameGrid
-my_grid = GameGrid()
-print(my_grid.grid)
